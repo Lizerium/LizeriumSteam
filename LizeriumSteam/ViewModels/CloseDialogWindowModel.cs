7db0c3f43999072e029ad900d1b8853a784a60b3
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-09 11:14:06 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-09

--- LizeriumSteam/ViewModels/CloseDialogWindowModel.cs
+++ LizeriumSteam/ViewModels/CloseDialogWindowModel.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 апреля 2026 14:44:03
- * Version: 1.0.13
+ * Last Updated: 09 апреля 2026 11:13:54
+ * Version: 1.0.14
  */
 
 using Prism.Commands;
