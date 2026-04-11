a39889249b70965a133a3e76d29c7c63c45587a3
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-11 13:55:15 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-11

--- LizeriumSteam/Models/Games/GameUpdate/ProcessUnpackFilesGamesModel.cs
+++ LizeriumSteam/Models/Games/GameUpdate/ProcessUnpackFilesGamesModel.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 апреля 2026 12:48:19
- * Version: 1.0.15
+ * Last Updated: 11 апреля 2026 13:55:06
+ * Version: 1.0.16
  */
 
 using AppUpdater.Server;
