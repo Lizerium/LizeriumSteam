016a394aecccaa5c380d2a15d556b865c43379db
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-15 07:05:17 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-15

--- LizeriumSteam/Events/Unpack/WarningLowTimeOperationEvent.cs
+++ LizeriumSteam/Events/Unpack/WarningLowTimeOperationEvent.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 апреля 2026 12:40:18
- * Version: 1.0.19
+ * Last Updated: 15 апреля 2026 07:05:07
+ * Version: 1.0.22
  */
 
 using Prism.Events;
