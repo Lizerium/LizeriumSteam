d909863664fffb143ee61baa966c232510a59d5d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-20 03:23:03 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-19

--- AppUpdater/LocalStructure/ILocalStructureManager.cs
+++ AppUpdater/LocalStructure/ILocalStructureManager.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 18 апреля 2026 15:00:38
- * Version: 1.0.25
+ * Last Updated: 20 апреля 2026 03:22:51
+ * Version: 1.0.26
  */
 
 using AppUpdater.LocalStructure.Components;
