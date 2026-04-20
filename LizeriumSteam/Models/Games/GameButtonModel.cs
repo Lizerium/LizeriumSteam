6c868361de0e95254013e5fc3036c6df47716f96
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-20 16:38:20 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-20

--- LizeriumSteam/Models/Games/GameButtonModel.cs
+++ LizeriumSteam/Models/Games/GameButtonModel.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 20 апреля 2026 03:22:51
- * Version: 1.0.26
+ * Last Updated: 20 апреля 2026 16:38:08
+ * Version: 1.0.27
  */
 
 using System.Collections.Generic;
