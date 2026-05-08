9d5d84467df2c1fc71e40986916880d1977b7c45
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-08 07:07:48 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-08

--- AppUpdater.Runner/Properties/AssemblyInfo.cs
+++ AppUpdater.Runner/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 мая 2026 15:46:52
- * Version: 1.0.44
+ * Last Updated: 08 мая 2026 07:07:38
+ * Version: 1.0.45
  */
 
 using System.Resources;
