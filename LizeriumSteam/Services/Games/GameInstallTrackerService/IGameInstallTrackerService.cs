3d449c7f50e8ab7603b07fe77d8eb3eb6cc2894e
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-12 12:23:18 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-12

--- LizeriumSteam/Services/Games/GameInstallTrackerService/IGameInstallTrackerService.cs
+++ LizeriumSteam/Services/Games/GameInstallTrackerService/IGameInstallTrackerService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 11 мая 2026 10:58:07
- * Version: 1.0.48
+ * Last Updated: 12 мая 2026 12:23:07
+ * Version: 1.0.49
  */
 
 namespace LizeriumSteam.Services.Games.GameInstallTrackerService
