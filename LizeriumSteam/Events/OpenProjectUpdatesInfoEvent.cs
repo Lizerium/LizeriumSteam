977c2edcfcb86ff0e6589d13c1b5ec52c7c0705c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-16 11:03:53 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-16

--- LizeriumSteam/Events/OpenProjectUpdatesInfoEvent.cs
+++ LizeriumSteam/Events/OpenProjectUpdatesInfoEvent.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 мая 2026 10:55:54
- * Version: 1.0.51
+ * Last Updated: 16 мая 2026 11:03:42
+ * Version: 1.0.52
  */
 
 using LizeriumSteam.Models.Games;
