c0a9204473bf1b5e332deb4b669261a9149faab4
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-17 11:32:17 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-17

--- LizeriumSteam/Models/Settings/ServerSettingsModel.cs
+++ LizeriumSteam/Models/Settings/ServerSettingsModel.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 16 мая 2026 11:03:42
- * Version: 1.0.52
+ * Last Updated: 17 мая 2026 11:32:05
+ * Version: 1.0.53
  */
 
 namespace LizeriumSteam.Models.Settings
