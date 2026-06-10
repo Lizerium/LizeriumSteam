5c88f12dfb3b0a7baf77679a3dfc08503289d83e
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-10 14:34:29 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-10

--- LizeriumSteam/Services/Games/GameSaveManagerService/IGameSaveManagerService.cs
+++ LizeriumSteam/Services/Games/GameSaveManagerService/IGameSaveManagerService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 июня 2026 17:13:27
- * Version: 1.0.76
+ * Last Updated: 10 июня 2026 14:34:16
+ * Version: 1.0.77
  */
 
 using System.Collections.Generic;
