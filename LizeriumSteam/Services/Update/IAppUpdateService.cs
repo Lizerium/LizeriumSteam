ea68c562b40c7e04eed5f96ceef519fdb5f389c6
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-19 07:11:24 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-19

--- LizeriumSteam/Services/Update/IAppUpdateService.cs
+++ LizeriumSteam/Services/Update/IAppUpdateService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 18 июня 2026 07:13:02
- * Version: 1.0.85
+ * Last Updated: 19 июня 2026 07:11:13
+ * Version: 1.0.86
  */
 
 namespace LizeriumSteam.Services.Update
