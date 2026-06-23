fe92f4ccddcf637f0cb98a6be75643fac7998904
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-23 16:15:48 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-23

--- LizeriumSteam/Events/StopManifestMonitorEvent.cs
+++ LizeriumSteam/Events/StopManifestMonitorEvent.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 22 июня 2026 07:14:10
- * Version: 1.0.89
+ * Last Updated: 23 июня 2026 16:15:35
+ * Version: 1.0.90
  */
 
 using Prism.Events;
