c5ffef4182175fc8e1761ab82386cc69d343aeb9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-26 07:12:03 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-26

--- LizeriumSteam/Services/Tray/Helps/UI/CustomToolStripRenderer.cs
+++ LizeriumSteam/Services/Tray/Helps/UI/CustomToolStripRenderer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 25 июня 2026 16:51:19
- * Version: 1.0.92
+ * Last Updated: 26 июня 2026 07:11:52
+ * Version: 1.0.93
  */
 
 using System.Drawing;
