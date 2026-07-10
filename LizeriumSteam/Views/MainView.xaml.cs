4956c6b428011f72c60718f975ec7e3cce30ef11
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-10 12:17:21 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-10

--- LizeriumSteam/Views/MainView.xaml.cs
+++ LizeriumSteam/Views/MainView.xaml.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 июля 2026 07:23:31
- * Version: 1.0.105
+ * Last Updated: 10 июля 2026 12:17:08
+ * Version: 1.0.106
  */
 using System;
 using System.ComponentModel;
