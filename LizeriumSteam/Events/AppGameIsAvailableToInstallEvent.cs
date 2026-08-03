288e2e27672fc79d58ef8aa1e9a1440bb23049b5
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-03 07:14:49 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-03

--- LizeriumSteam/Events/AppGameIsAvailableToInstallEvent.cs
+++ LizeriumSteam/Events/AppGameIsAvailableToInstallEvent.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 02 августа 2026 07:14:12
- * Version: 1.0.129
+ * Last Updated: 03 августа 2026 07:14:35
+ * Version: 1.0.130
  */
 
 using LizeriumSteam.Models.Games;
