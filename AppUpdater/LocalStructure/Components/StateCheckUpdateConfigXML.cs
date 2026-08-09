280758c7197d00e6686f5ec817405b1e7bca0906
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-09 15:53:11 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-09

--- AppUpdater/LocalStructure/Components/StateCheckUpdateConfigXML.cs
+++ AppUpdater/LocalStructure/Components/StateCheckUpdateConfigXML.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 августа 2026 07:14:15
- * Version: 1.0.135
+ * Last Updated: 09 августа 2026 15:52:58
+ * Version: 1.0.136
  */
 
 namespace AppUpdater.LocalStructure.Components
