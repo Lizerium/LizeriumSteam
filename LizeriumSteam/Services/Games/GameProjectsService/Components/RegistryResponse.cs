81fadf943aa2f22ef0e494a9fa91a7a3303dda90
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-12 07:14:11 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-12

--- LizeriumSteam/Services/Games/GameProjectsService/Components/RegistryResponse.cs
+++ LizeriumSteam/Services/Games/GameProjectsService/Components/RegistryResponse.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 11 августа 2026 07:13:32
- * Version: 1.0.138
+ * Last Updated: 12 августа 2026 07:13:58
+ * Version: 1.0.139
  */
 
 namespace LizeriumSteam.Services.Games.GameProjectsService.Components
