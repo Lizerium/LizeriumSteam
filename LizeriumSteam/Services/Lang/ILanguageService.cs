349127ccd1f22dedc1069eac4d9576217d798e22
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-13 07:11:50 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-13

--- LizeriumSteam/Services/Lang/ILanguageService.cs
+++ LizeriumSteam/Services/Lang/ILanguageService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 12 августа 2026 07:13:58
- * Version: 1.0.139
+ * Last Updated: 13 августа 2026 07:11:38
+ * Version: 1.0.140
  */
 
 namespace LizeriumSteam.Services.Lang
