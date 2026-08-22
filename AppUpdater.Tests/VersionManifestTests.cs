9f806e12a677259342b53ea9dae73bfd8e83c7be
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-22 15:02:52 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-22

--- AppUpdater.Tests/VersionManifestTests.cs
+++ AppUpdater.Tests/VersionManifestTests.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 21 августа 2026 07:15:33
- * Version: 1.0.148
+ * Last Updated: 22 августа 2026 15:02:37
+ * Version: 1.0.149
  */
 
 using System.Linq;
