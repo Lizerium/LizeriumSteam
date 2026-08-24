542c52b29443d37dd9ba8a902c66cd5a6ab14320
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-24 07:15:03 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-24

--- LizeriumSteam/Services/Tray/Windows/ITrayAlertsService.cs
+++ LizeriumSteam/Services/Tray/Windows/ITrayAlertsService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 23 августа 2026 07:15:07
- * Version: 1.0.150
+ * Last Updated: 24 августа 2026 07:14:49
+ * Version: 1.0.151
  */
 
 using AppUpdater.Chef;
