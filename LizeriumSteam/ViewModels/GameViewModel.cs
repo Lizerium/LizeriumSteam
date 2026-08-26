664581439d0d522ce94081a6e0c1b497c2497ffb
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-26 07:14:57 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-26

--- LizeriumSteam/ViewModels/GameViewModel.cs
+++ LizeriumSteam/ViewModels/GameViewModel.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 25 августа 2026 07:14:31
- * Version: 1.0.152
+ * Last Updated: 26 августа 2026 07:14:44
+ * Version: 1.0.153
  */
 
 using System;
