a04375bdcd0e98a590d28f967bf44052afb35873
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-06 11:14:01 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-06

--- AppUpdater/Chef/IUpdaterChef.cs
+++ AppUpdater/Chef/IUpdaterChef.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 05 сентября 2026 08:57:39
- * Version: 1.0.162
+ * Last Updated: 06 сентября 2026 11:13:48
+ * Version: 1.0.163
  */
 
 using AppUpdater.Recipe;
