e55ac7f6cbf5a10a053bfafdb91f8741aa3af70d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-12 06:58:04 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-12

--- AppUpdater.Tests/Server/ExtractZipTests.cs
+++ AppUpdater.Tests/Server/ExtractZipTests.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 11 сентября 2026 06:57:47
- * Version: 1.0.168
+ * Last Updated: 12 сентября 2026 06:57:51
+ * Version: 1.0.169
  */
 
 using AppUpdater.Publisher;
