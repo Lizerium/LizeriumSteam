72e5c10bf12ae581006065ed0dc14b091da47327
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-17 07:00:09 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-17

--- AppUpdater/Server/DataDownloadHandle.cs
+++ AppUpdater/Server/DataDownloadHandle.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 16 сентября 2026 10:38:00
- * Version: 1.0.173
+ * Last Updated: 17 сентября 2026 06:59:56
+ * Version: 1.0.174
  */
 
 namespace AppUpdater.Server
