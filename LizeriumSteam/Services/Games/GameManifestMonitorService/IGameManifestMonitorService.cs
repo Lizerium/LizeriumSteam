672ef36fe5556f7aebbf304d3e41c574a7175b88
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-18 08:19:33 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-18

--- LizeriumSteam/Services/Games/GameManifestMonitorService/IGameManifestMonitorService.cs
+++ LizeriumSteam/Services/Games/GameManifestMonitorService/IGameManifestMonitorService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 17 сентября 2026 06:59:56
- * Version: 1.0.174
+ * Last Updated: 18 сентября 2026 08:19:20
+ * Version: 1.0.175
  */
 
 namespace LizeriumSteam.Services.Games.GameManifestMonitorService
